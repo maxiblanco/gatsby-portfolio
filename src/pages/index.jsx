9e901ed3294c02bd924d6527ac932f04182541db
Maximiliano Blanco <inkcoming@protonmail.com>
2020-07-01 10:23:49 -0300

    Align content and colors. Hide togggle

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
-/* import Fade from 'react-reveal/Fade'; */
+import Fade from 'react-reveal/Fade';
 // Styles
 import 'normalize.css';
 import '../styles/index.scss';
@@ -26,8 +26,14 @@ const Index = () => {
           <title>Maximiliano Blanco</title>
         </Helmet>
         <header>
-          <FontAwesomeIcon icon={['fas', 'toggle-off']} />
-          <FontAwesomeIcon icon={['fas', 'toggle-on']} />
+          <div className="toggle">
+            <FontAwesomeIcon
+              icon={['fas', 'toggle-off']}
+              width="100px"
+              height="100px"
+            />
+            <FontAwesomeIcon icon={['fas', 'toggle-on']} />
+          </div>
         </header>
         <section>
           <div className="title--container">
@@ -43,37 +49,46 @@ const Index = () => {
                   <li>Proyects</li>
                   <li>Blog</li>
                   <li>Contact</li>
-                  <li>CV</li>
+                  <li>
+                    {' '}
+                    <a
+                      download
+                      href="../assets/pdf/CV_MaximilianoBlanco_ENG.pdf"
+                    >
+                      CV
+                    </a>
+                  </li>
                 </ul>
               </nav>
             </div>
           </div>
-
-          <div className="message--container">
-            <div className="message--content">
-              <p>Every interaction tells a story.</p>
-              <p>
-                I’m interested in how technology influences the stories we tell
-                each other.
-              </p>
-              <p>
-                I strive develop software that makes each story more engaging.
-              </p>
-              <p>
-                I currently live in Villa Crespo, a neighborhood of Buenos
-                Aires, Argentina.
-              </p>
+          <Fade slower>
+            <div className="message--container">
+              <div className="message--content">
+                <p>Every interaction tells a story.</p>
+                <p>
+                  I’m interested in how technology influences the stories we
+                  tell each other.
+                </p>
+                <p>
+                  I strive develop software that makes each story more engaging.
+                </p>
+              </div>
             </div>
-          </div>
+          </Fade>
         </section>
         <footer>
           <div className="footer--container">
             <div className="footer--content">
               <div className="social-links">
-                <FontAwesomeIcon icon={['fab', 'linkedin']} />
-                <FontAwesomeIcon icon={['fab', 'github']} />
+                <a href="https://www.linkedin.com/in/maxiblanco/">
+                  <FontAwesomeIcon icon={['fab', 'linkedin']} />
+                </a>
+                <a href="https://github.com/maxiblanco">
+                  <FontAwesomeIcon icon={['fab', 'github']} />
+                </a>
               </div>
-              <p>Maximiliano Blanco &#169; 2020</p>
+              <p>Work in progress by Maximiliano Blanco &#169; 2020</p>
             </div>
           </div>
         </footer>
