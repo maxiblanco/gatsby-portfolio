eb0b5f4ecfe0d8a46c6673081d3ebba00bff6b82
Maximiliano Blanco <inkcoming@protonmail.com>
2020-06-30 21:51:30 -0300

    Work on base styles and html

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
-import Fade from 'react-reveal/Fade';
+/* import Fade from 'react-reveal/Fade'; */
 // Styles
 import 'normalize.css';
 import '../styles/index.scss';
@@ -8,7 +8,8 @@ import '../styles/index.scss';
 import '../utils/fontawesome';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function Index() {
+const Index = () => {
+  /*   const [darkMode, toggleDarkMode] = useState(false); */
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
@@ -20,50 +21,63 @@ function Index() {
   }, []);
   return (
     <main>
-      <Helmet>
-        <title>Maximiliano Blanco</title>
-      </Helmet>
-      <section>
-        <Fade right>
-          <h1>Maximiliano Blanco - Portfolio</h1>
-          <p>Convincing text, now you will want to hire me.</p>
-        </Fade>
-      </section>
-      <section>
-        <h2>Proyect</h2>
-        <FontAwesomeIcon icon={['fab', 'linkedin']} />
-        <div className="container">
-          <div className="content">
-            <Fade bottom>
-              <img
-                src="https://via.placeholder.com/300.png?text=Proyect"
-                alt="1"
-              />
-            </Fade>
-            <Fade top>
-              <img
-                src="https://via.placeholder.com/300.png?text=Proyect"
-                alt="2"
-              />
-            </Fade>
-
-            <Fade left>
-              <img
-                src="https://via.placeholder.com/300.png?text=Proyect"
-                alt="3"
-              />
-            </Fade>
-            <Fade top>
-              <img
-                src="https://via.placeholder.com/300.png?text=Proyect"
-                alt="4"
-              />
-            </Fade>
+      <div className="main--container center">
+        <Helmet>
+          <title>Maximiliano Blanco</title>
+        </Helmet>
+        <header>
+          <FontAwesomeIcon icon={['fas', 'toggle-off']} />
+          <FontAwesomeIcon icon={['fas', 'toggle-on']} />
+        </header>
+        <section>
+          <div className="title--container">
+            <div className="title--content">
+              <h1>Hi! I'm Max.</h1>
+              <h1>A Web Developer.</h1>
+            </div>
           </div>
-        </div>
-      </section>
+          <div className="nav--container">
+            <div className="nav--content">
+              <nav>
+                <ul>
+                  <li>Proyects</li>
+                  <li>Blog</li>
+                  <li>Contact</li>
+                  <li>CV</li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div className="message--container">
+            <div className="message--content">
+              <p>Every interaction tells a story.</p>
+              <p>
+                I’m interested in how technology influences the stories we tell
+                ourselves.
+              </p>
+              <p>I want to develop software to make them more engaging.</p>
+              <p>
+                I currently live in Villa Crespo, a neighborhood of Buenos
+                Aires, Argentina.
+              </p>
+            </div>
+          </div>
+        </section>
+        <footer>
+          <div className="footer--container">
+            <div className="footer--content">
+              <div className="social-links">
+                <FontAwesomeIcon icon={['fab', 'linkedin']} />
+                <FontAwesomeIcon icon={['fab', 'github']} />
+              </div>
+              <p>Maximiliano Blanco &#169; 2020</p>
+            </div>
+          </div>
+        </footer>
+      </div>
     </main>
   );
-}
+};
 
 export default Index;
--- src/utils/fontawesome.js
+++ src/utils/fontawesome.js
@@ -1,4 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faLinkedin);
+library.add(faLinkedin, faGithub, faToggleOn, faToggleOff);
