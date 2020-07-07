e213b78decf7624e69e8b5cdaf69da36cdbb349f
Maximiliano Blanco <inkcoming@protonmail.com>
2020-07-06 23:41:13 -0300

    Add fontaweson icons for projects

--- src/components/ProyectCard.jsx
+++ src/components/ProyectCard.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ProyectCard = ({ image = {}, title, description, technologies = {} }) => (
   <article className="card">
     <div className="card-header">
-      <div className="card-header-title">
+      <div className="card-header-title is-centered">
         <p>{title}</p>
       </div>
     </div>
-    <div className="card-image">
-      <figure>
-        <img src={image.src} alt={image.alt} width="200px" />
-      </figure>
+    <div className="columns is-vcentered">
+      <div className="column">
+        <div className="card-image mx-4">
+          <figure>
+            <img src={image.src} alt={image.alt} width="200px" />
+          </figure>
+        </div>
+      </div>
+      <div className="column ">
+        <div className="card-content">
+          <p>{description}</p>
+          <span className="icon is-large">
+            <FontAwesomeIcon
+              icon={['fab', 'js']}
+              className="mx-3 my-3 is-size-3"
+            />
+          </span>
+          <span className="icon is-large">
+            <FontAwesomeIcon
+              icon={['fab', 'node']}
+              className="mx-3 my-3 is-size-3"
+            />
+          </span>
+          <span className="icon is-large">
+            <FontAwesomeIcon
+              icon={['fab', 'react']}
+              className="mx-3 my-3 is-size-3"
+            />
+          </span>
+        </div>
+      </div>
     </div>
-    <div className="card-content">
-      <p>{description}</p>
-    </div>
+
     <footer className="card-footer">
       <button type="button" className="button card-footer-item">
         Code
