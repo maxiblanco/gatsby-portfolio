a50a7f09319decc7b3d561eef0619b349708ccfb
Maximiliano Blanco <inkcoming@protonmail.com>
2020-07-06 23:40:05 -0300

    Add icons for tech

--- src/utils/fontawesome.js
+++ src/utils/fontawesome.js
@@ -1,5 +1,19 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import {
+  faLinkedin,
+  faGithub,
+  faNode,
+  faJs,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons';
 import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faLinkedin, faGithub, faToggleOn, faToggleOff);
+library.add(
+  faLinkedin,
+  faGithub,
+  faToggleOn,
+  faToggleOff,
+  faNode,
+  faJs,
+  faReact
+);
