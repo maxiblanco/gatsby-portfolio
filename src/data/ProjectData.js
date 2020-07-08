8e674a78f271dfb07631e9bada81fba20c8e97f7
Maximiliano Blanco <inkcoming@protonmail.com>
2020-07-08 13:23:06 -0300

    Move out project data to own file

---
+++ src/data/ProjectData.js
@@ -0,0 +1,43 @@
+export const healthyProjectData = {
+  image: {
+    src: 'https://media.giphy.com/media/chE11KhoG6Y7ysmfMc/giphy.gif',
+    alt: 'Healthy Dev Web App',
+  },
+  title: 'Healthy Dev Web App',
+  description:
+    'Web app to share news and tips. Community development with mentor guidance',
+  technologies: ['react', 'redux', 'redux-sagas'],
+};
+
+export const travelJournalProjectData = {
+  image: {
+    src: 'https://media.giphy.com/media/dyRfopPsLDHk2SwfOK/giphy.gif',
+    alt: 'Travel Journal Web App',
+  },
+  title: 'Travel Journal Web App',
+  description:
+    'Web app to share travel stories built with Node.js and Express.',
+  technologies: ['Node.js', 'Express', 'javascript'],
+};
+
+export const chatbotProjectData = {
+  image: {
+    src: 'https://media.giphy.com/media/Kfl1NveFi8QAG9MZHF/giphy.gif',
+    alt: 'Chatbot image',
+  },
+  title: 'Firebot',
+  description:
+    'A chatbot connected to an Api that returns random stories and dialog.',
+  technologies: ['react', 'javascript', 'node'],
+};
+
+export const travelMapProjectData = {
+  image: {
+    src: 'https://media.giphy.com/media/hVIUhNFlzZjobrwkZy/giphy.gif',
+    alt: 'Travel Map',
+  },
+  title: 'Travel Map',
+  description:
+    'A Map connected to an api to store locations with comments on a map.',
+  technologies: ['react', 'node', 'mapbox'],
+};
